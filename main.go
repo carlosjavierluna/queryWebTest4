b93866bb5bbec2b7515c3bb16c372edbbff55d3a
Carlos Luna <carlos.javier.luna@gmail.com>
2022-03-22 18:41:07 +0100

    Version solamente con numeros. Antes de cambiar a ciudades.

--- main.go
+++ main.go
@@ -18,7 +18,6 @@ type numx struct {
 }
 
 func main() {
-
 	// se definen un canal bidireccional
 	canal := make(chan numx)
 	// Se declara un arreglo de estructuras
@@ -28,20 +27,23 @@ func main() {
 
 	fmt.Println("Numeros antes del lazo for: ", numeros) // muestra como se creo el arreglo de numeros.
 
-	// Se puede llamar a la funcion llenar_arreglos
-	// antes ya que la funcion espera a que
-	// el canal tenga datos.
-	go llenar_arreglo(&numeros, canal)
-
 	// USamos la go rutina para que los numeros se guarden
 	// en el canal en cualquier orden.
 	for i := 0; i < 20; i++ {
 		go llenar_canal(canal, i)
 	}
 
-	time.Sleep(15 * time.Millisecond)         // Se pone este delay para esperar que terminen todas las go rutinas.
-	fmt.Println("Despues del for: ", numeros) // se imprime el slice de numeros luego de llamar a las go rutinas.
-	texto := fmt.Sprintf("Largo del arreglo: %d Capacidad del arreglo: %d", len(numeros), cap(numeros))
+	// Se puede llamar a la funcion llenar_arreglos
+	// antes ya que la funcion espera a que
+	// el canal tenga datos.
+	go llenar_arreglo(&numeros, canal)
+
+	time.Sleep(10 * time.Millisecond)    // Se pone este delay para esperar que terminen todas las go rutinas.
+	fmt.Println("\nDespues del Go...\n") // se imprime el slice de numeros luego de llamar a las go rutinas.
+	for i := range numeros {
+		fmt.Println("Arreglo [", i, "] ", numeros[i])
+	}
+	texto := fmt.Sprintf("\nLargo del arreglo: >>> %d <<< Capacidad del arreglo: >>> %d <<<\n", len(numeros), cap(numeros))
 	fmt.Println(texto)
 }
 
@@ -59,12 +61,13 @@ func llenar_canal(ch chan<- numx, i int) {
 // y le aniade una nuevo elemento considerando si el parametro
 // que obtiene del canal  valor de (i)
 // es par o impar.
-func llenar_arreglo(x *[]numx, ch <-chan numx) { // la declaracion es un puntero al un slice de numx
+func llenar_arreglo(x *[]numx, ch <-chan numx) {
+	// la declaracion es un puntero al un slice de numx
 	//fmt.Println("Valor de i:",i)
 	for {
 		i := <-ch // Se recupera el dato del canal
 
-		fmt.Println(i)
+		fmt.Println("i dentro de llenar_arreglo():", i)
 
 		// Se determina si es par o impar
 		if i.idx%2 == 0 {
